ebe4532028049ef983bde1025f61286233cb9cae
Mahesh <maheshmhaske241198@gmail.com>
2023-06-16 16:37:34 +0530

    ok:

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use('/blog', express.static(path.join(__dirname, 'public/blog'))) // PROFILE IMAGES
+
 app.use("/", indexRouter);
 app.use("/api/admin/blog", blogRouter);
 app.use("/api/user", userRouter);
