6e91c26c744e2f3736e3804567c38b1ac679fb29
Mahesh <maheshmhaske241198@gmail.com>
2023-06-21 16:51:00 +0530

    ok:

--- controller/blog.js
+++ controller/blog.js
@@ -2,228 +2,195 @@ const db = require("../db");
 const fs = require("fs");
 const { uploadBlogImg } = require("../middleware/upload");
 const { checkMandatoryFields } = require("../middleware/validators");
-const { error } = require("console");
+const SQL = require('../middleware/sqlQueryHandler')
 
 exports.addBlog = async (req, res) => {
-  const { title, url, description, route, image, tag, date, sections } =
-    req.body;
 
-  const query = `
-    INSERT INTO xx_blog 
-    SET 
-      tag = ?, 
-      route = ?, 
-      title = ?, 
-      url = ?, 
-      description = ?, 
-      image = ?, 
-      date = ?
-  `;
-  const values = [tag, route, title, url, description, image, date];
 
-  await db.query(query, values, async (error, response) => {
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-        error: error,
-      });
-    }
+  try {
+    const { title, url, description, route, image, tag, date, sections } = req.body;
 
-    let blogid = response.insertId
-    let addSectionQuery = "";
-    sections.forEach((section) => {
-      addSectionQuery += `INSERT INTO xx_blog_details (blogid, heading, section, image, sort) VALUES (${response.insertId}, '${section.heading}', '${section.section}', '${section.image}', ${section.sort}); `;
-    });
-    console.log("addSectionQuery ==>",addSectionQuery)
-    await db.query(addSectionQuery, (error, response) => {
+    SQL.insert('xx_blog', { title, url, description, route, image, tag, date }, (error, results) => {
       if (error) {
         return res.json({
           status: false,
-          message: "something went wrong",
-          error: error,
-        });
+          error: error
+        })
       }
-      else {
+
+      if (results.affectedRows > 0) {
+        const blogId = results.insertId
+        sections.forEach(async (section) => {
+          section["blogid"] = results.insertId
+          await SQL.insert('xx_blog_details', section, (error, results) => {
+            if (error) {
+              return res.json({
+                status: false,
+                error: error
+              })
+            }
+          })
+        })
         return res.json({
           status: true,
-          message: "Blog added successfully",
-          data: { id: blogid }
-        });
+          message: 'blog added',
+          data: { blogId:blogId}
+        })
       }
     });
-
-    // try {
-    //   return res.json({
-    //     status: true,
-    //     message: "Blog added successfully",
-    //   });
-    // } catch (error) {
-    //   return res.json({
-    //     status: false,
-    //     message: "Something went wrong",
-    //     error: error,
-    //   });
-    // }
-  });
+  }
+  catch (error) {
+    return res.json({
+      status: false,
+      message: "something went wrong",
+      error: error
+    })
+  }
 };
 
 exports.getBlogs = async (req, res) => {
-  const query = `
-SELECT 
-  * 
-FROM 
-  xx_blog 
-  JOIN xx_blog_details ON xx_blog.id = xx_blog_details.blogid
-  `;
-
-  await db.query(query, (error, response) => {
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-        error: error,
-      });
-    } else {
+  try {
+    SQL.get(`xx_blog`, ``, ``, (error, results) => {
+      if (error) {
+        return res.json({
+          status: false,
+          error: error
+        })
+      }
       return res.json({
         status: true,
         message: "blog details",
-        data: response,
-      });
-    }
-  });
-};
-
-exports.removeBlog = async (req, res) => {
-  const { blogId } = req.params;
-  await checkMandatoryFields(blogId);
-
-  const query = `
-  DELETE FROM xx_blog WHERE id = ?;
-  DELETE FROM xx_blog_details WHERE blogid =?;
-`;
-  const values = [blogId, blogId];
-
-  await db.query(query, values, (error, response) => {
-    console.log(response);
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-        error,
-      });
-    } else if (response[0].affectedRows > 0) {
-      return res.json({
-        status: true,
-        message: "Blog removed successfully",
-      });
-    } else if (response[0].affectedRows == 0) {
-      return res.json({
-        status: true,
-        message: "please provide valid blogId",
-      });
-    }
-  });
+        data: results
+      })
+    });
+  }
+  catch (error) {
+    return res.json({
+      status: false,
+      message: "something went wrong",
+      error: error
+    })
+  }
 };
 
 exports.editBlog = async (req, res) => {
-  const { blogId } = req.params;
-  let update_data = req.body;
+  try {
+    const { blogId } = req.params
+    const update_data = req.body
 
-  const query = `UPDATE xx_blog SET ? WHERE id=${blogId}`;
-  const values = [update_data]
-
-  db.query(query, values, async (error, response) => {
-    if (error) {
+    if (update_data.id) {
       return res.json({
         status: false,
-        message: "something went wrong",
-        error,
-      });
+        message: "id cannot be edit"
+      })
     }
 
-    if (response.affectedRows > 0) {
-      return res.json({
-        status: true,
-        message: "record updated successfully",
-      });
-    } else if (response.affectedRows == 0) {
-      return res.json({
-        status: false,
-        message: "invalid blogid id",
-      });
-    }
-  });
+    SQL.update('xx_blog', update_data, `id=${blogId}`, (error, results) => {
+      if (error) {
+        return res.json({
+          status: false,
+          error: error
+        })
+      }
+      if (results.affectedRows > 0) {
+        return res.json({
+          status: true,
+          message: 'blog details updated successfully'
+        })
+      }
+    })
+  }
+  catch (error) {
+    return res.json({
+      status: false,
+      message: "something went wrong",
+      error: error
+    })
+  }
 };
 
 exports.getBlog = async (req, res) => {
-  const { blogId } = req.params;
-  await checkMandatoryFields({ blogId });
-
-  const query = `
-  SELECT * FROM xx_blog
-  JOIN xx_blog_details 
-  ON xx_blog.id = xx_blog_details.blogid where xx_blog.id =?;
-  `;
-  await db.query(query, [blogId], (error, response) => {
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-        error: error,
-      });
-    } else {
+  try {
+    const blogId = req.params.blogId;
+    SQL.get('xx_blog', '', `id=${blogId}`, (error, results) => {
+      if (error) {
+        return res.json({
+          status: false,
+          error: error
+        })
+      }
       return res.json({
         status: true,
         message: "blog details",
-        data: response,
-      });
-    }
-  });
+        data: results
+      })
+    });
+  }
+  catch (error) {
+    return res.json({
+      status: false,
+      message: "something went wrong",
+      error: error
+    })
+  }
 };
 
 exports.getAllBlogTags = async (req, res) => {
-  const query = `SELECT * FROM xx_blog_tag`;
-
-  await db.query(query, (error, response) => {
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-        error: error,
-      });
-    } else {
+  try {
+    SQL.get('xx_blog_tag', '', ``, (error, results) => {
+      if (error) {
+        return res.json({
+          status: false,
+          error: error
+        })
+      }
       return res.json({
         status: true,
-        message: "blog tags",
-        data: response,
-      });
-    }
-  });
+        message: "blog tags details",
+        data: results
+      })
+    });
+  }
+  catch (error) {
+    return res.json({
+      status: false,
+      message: "something went wrong",
+      error: error
+    })
+  }
 };
 
 exports.addBlogTag = async (req, res) => {
-  const { site, tag, url, title } = req.body;
-  await checkMandatoryFields(res, { site, tag, url, title });
+  try {
 
-  const query = `INSERT INTO xx_blog_tag SET site=?,tag=?, url=?,title=?`;
-  const values = [site, tag, url, title];
+    const { site, tag, url, title } = req.body;
+    await checkMandatoryFields(res, { site, tag, url, title });
 
-  await db.query(query, values, (error, response) => {
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-        error: error,
-      });
-    } else if (response.affectedRows > 0) {
-      return res.json({
-        status: true,
-        message: "tag added",
-      });
-    }
-  });
+    SQL.insert('xx_blog_tag', req.body, (error, results) => {
+      if (error) {
+        return res.json({
+          status: false,
+          error: error
+        })
+      }
+      if (results.affectedRows > 0) {
+        return res.json({
+          status: true,
+          message: 'tags added successfully'
+        })
+      }
+    });
+  }
+  catch (error) {
+    return res.json({
+      status: false,
+      message: "something went wrong",
+      error: error
+    })
+  }
 };
+
+// ============ BLOG IMAGES ============ //
 
 exports.addBlogImage = async (req, res) => {
   uploadBlogImg(req, res, function (error) {
@@ -263,121 +230,67 @@ exports.deleteBlogImage = async (req, res) => {
 };
 
 // ============ SECTION APIS ============ //
-exports.removeBlogSection = async (req, res) => {
-  const { sectionId } = req.params;
-
-  if (!sectionId) {
-    return res.json({
-      status: false,
-      message: "please provide section id",
-    });
-  }
-
-  const query = `DELETE FROM xx_blog_details WHERE id=?`;
-  const values = [sectionId];
-  await db.query(query, values, (error, response) => {
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-      });
-    } else if (response.affectedRows > 0) {
-      return res.json({
-        status: true,
-        message: "section removed successfully",
-      });
-    } else if (response.affectedRows == 0) {
-      return res.json({
-        status: true,
-        message: "please provide valid sectionId",
-      });
-    }
-  });
-};
 
 exports.editSection = async (req, res) => {
-  const { sectionId } = req.params;
-  let { heading, sort, image, section, date, site } = req.body;
+  try {
+    const { sectionId } = req.params;
+    const update_data = req.body
 
-  const query = `UPDATE xx_blog_details SET heading=? ,sort=? ,image=? ,section=? ,date=? ,site=?  WHERE id=${sectionId}`;
-  const values = [
-    heading,
-    sort,
-    image,
-    section,
-    date,
-    (site = site == undefined ? (site = "") : site),
-    sectionId,
-  ];
-
-  const completeQuery = query.replace(/\?/g, (m) => {
-    const value = values.shift();
-    return typeof value === "string" ? `'${value}'` : value;
-  });
-
-  db.query(completeQuery, async (error, response) => {
-    if (error) {
+    if (update_data.id) {
       return res.json({
         status: false,
-        message: "something went wrong",
-        error,
-      });
+        message: "id cannot be edit"
+      })
     }
 
-    if (response.affectedRows > 0) {
-      return res.json({
-        status: true,
-        message: "record updated successfully",
-      });
-    } else if (response.affectedRows == 0) {
-      return res.json({
-        status: false,
-        message: "invalid section id",
-      });
-    }
-  });
+    SQL.update('xx_blog_details', update_data, `id=${sectionId}`, (error, results) => {
+      if (error) {
+        return res.json({
+          status: false,
+          error: error
+        })
+      }
+      if (results.affectedRows > 0) {
+        return res.json({
+          status: true,
+          message: 'section details updated successfully'
+        })
+      }
+    })
+  }
+  catch (error) {
+    return res.json({
+      status: false,
+      message: "something went wrong",
+      error: error
+    })
+  }
 };
 
 exports.getSectionByBlog = async (req, res) => {
-  const { blogId } = req.params;
-  await checkMandatoryFields({ blogId });
-
-  const query = `SELECT * FROM xx_blog_details where blogid=?`;
-  await db.query(query, [blogId], (error, response) => {
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-        error: error,
-      });
-    } else {
+  try {
+    const { blogId } = req.params;
+    await checkMandatoryFields({ blogId });
+    SQL.get('xx_blog_details', '', `id=${blogId}`, (error, results) => {
+      if (error) {
+        return res.json({
+          status: false,
+          error: error
+        })
+      }
       return res.json({
         status: true,
-        message: "blog section details",
-        data: response,
-      });
-    }
-  });
+        message: "blog sections details",
+        data: results
+      })
+    });
+  }
+  catch (error) {
+    return res.json({
+      status: false,
+      message: "something went wrong",
+      error: error
+    })
+  }
 };
 
-exports.getSection = async (req, res) => {
-  const { sectionId } = req.params;
-  await checkMandatoryFields({ sectionId });
-
-  const query = `SELECT * FROM xx_blog_details join xx_blog on xx_blog.id = xx_blog_details.blogid where xx_blog_details.id=?`;
-  await db.query(query, [sectionId], (error, response) => {
-    if (error) {
-      return res.json({
-        status: false,
-        message: "something went wrong",
-        error: error,
-      });
-    } else {
-      return res.json({
-        status: true,
-        message: "section details",
-        data: response,
-      });
-    }
-  });
-};
--- routes/blog.js
+++ routes/blog.js
@@ -12,7 +12,6 @@ router.post("/add", blogController.addBlog);
 router.get("/get", blogController.getBlogs);
 router.get("/get/:blogId", blogController.getBlog);
 router.put("/edit/:blogId", blogController.editBlog);
-router.delete("/delete/:blogId", blogController.removeBlog);
 router.post("/addImg", blogController.addBlogImage);
 router.delete("/deleteImg/:blogImage", blogController.deleteBlogImage);
 router.get("/tag/getAll", blogController.getAllBlogTags);
@@ -20,8 +19,6 @@ router.post("/tag/add", blogController.addBlogTag);
 
 // ============================== section api ============================== //
 router.put("/section/update/:sectionId", blogController.editSection);
-router.delete("/section/delete/:sectionId", blogController.removeBlogSection);
 router.get("/section/getByBlog/:blogId", blogController.getSectionByBlog);
-router.get("/section/get/:sectionId", blogController.getSection);
 
 module.exports = router;
