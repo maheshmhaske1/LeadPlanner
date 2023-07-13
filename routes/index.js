7a24720845c9d60661acc1ba61f67917be6c547c
Mahesh <maheshmhaske241198@gmail.com>
2023-07-13 16:49:52 +0530

    pk

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,9 @@ router.get('/', function (req, res, next) {
 
 // res.render('index', { title: 'Express' });
 router.get('/api/test', function (req, res, next) {
+  console.log("req.session.userId ==>",req.session.userId)
   if (!req.session.userId) {
+    console.log("req.session.userId in if ==>",req.session.userId)
     return res.json({
       message: 'please login'
     })
