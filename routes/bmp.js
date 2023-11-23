d318a6c04d232b915af4f15d8b8a9c9a5fef65c7
Mahesh <maheshmhaske241198@gmail.com>
2023-11-23 15:28:30 +0530

    ok:

--- routes/bmp.js
+++ routes/bmp.js
@@ -25,7 +25,7 @@ router.post('/academy/getreviews', bmp.getTotalReviews)
 router.post('/academy/getreviewsbytype', bmp.getAllReviewsByType)
 router.put('/academy/review/update/:review_id',auth.verifyToken, bmp.updateReview)
 router.post('/academy/review/reply', verifyBmpAdminOrBmpAcademyManager, bmp.addReviewReply)
-router.post('/academy/getreviewreply', verifyBmpAdminOrBmpAcademyManager, bmp.getReviewReply)
+router.post('/academy/getreviewreply', bmp.getReviewReply)
 router.post('/academy/getreviewreport', verifyBmpAdminOrBmpAcademyManager, bmp.getReviewReport)
 
 // ============== awards ============= //
