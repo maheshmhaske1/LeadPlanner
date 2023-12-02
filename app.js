3e13f7974659b68e6662f3145892413c6547373c
Mahesh mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2023-12-02 11:04:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan")
+var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var blogRouter = require("./routes/blog");
