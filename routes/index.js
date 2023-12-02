3cfabe6995e9f7d4eee8a627ed840ea08ba90339
Mahesh mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2023-12-02 12:54:07 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World --+> 1!');
+    res.send('Welcome to Leadplanner');
 })
 
 module.exports = router;
