5f8fa975269a7b4f01c8da342809a486505de6b0
mahesh <maheshmhaske241198@gmail.com>
2024-01-02 16:25:50 +0000

    ok

--- routes/ezuka.js
+++ routes/ezuka.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const ezukaController = require('../controller/ezuka')
 
 
-router.get('/contact/create', ezukaController.createContact)
+router.post('/contact/create', ezukaController.createContact)
 
 module.exports = router;
